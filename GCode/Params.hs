f3eabcc3b9fc5ec995336503252fe2cd0f9f4294
moxuse <moxuse@gmail.com>
2018-03-29 18:38:22 +0900

    sinX, sinY

--- GCode/Params.hs
+++ GCode/Params.hs
@@ -18,17 +18,20 @@ pt t = truncateTrail (truncateTrail t)
 ptSin :: Double -> [Trail] -> [Trail]
 ptSin rad xs = map (\ x -> pt ((sin (fst x)) * rad, (sin (snd x)) * rad)) xs
 
-generateX n = [(x, 0) | x <- [0..n]]
+generateX n = map (pt) [(x, 0) | x <- [0..n]]
 
-generateY n = [(0, y) | y <- [0..n]]
+generateY n = map (pt) [(0, y) | y <- [0..n]]
 
 translate' :: (Double, Double) -> [Trail] -> [Trail]
-translate' t ls = map (\ l -> ((fst l) + (fst t), (snd l) + (snd t))) ls
+translate' t ls = map (\ l -> pt ((fst l) + (fst t), (snd l) + (snd t))) ls
 
-sin' :: Double -> [Trail] -> [Trail]
-sin' rad = map (\ l -> ((fst l), (snd l))) ls
+sinX :: Double -> [Trail] -> [Trail]
+sinX rad ls = map (\ l -> pt (sin (fst l) * rad, (snd l))) ls
+
+sinY :: Double -> [Trail] -> [Trail]
+sinY rad ls = map (\ l -> pt ((fst l), sin (snd l) * rad)) ls
 
 randomize :: Double -> [Trail] -> [Trail]
-randomize fact ls = map (\ l -> ((randp 1 fact) + (fst l), (randp (2 + 8) fact) + (snd l) )) ls
+randomize fact ls = map (\ l -> pt ((randp 1 fact) + (fst l), (randp (2 + 8) fact) + (snd l) )) ls
 
 randp s mx = head $ take 1 $ randomRs (0, mx) (mkStdGen s) :: Double
