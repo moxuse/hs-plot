291738f96b6a3a7929d0c6da6f1a407ec277d262
moxuse <moxuse@gmail.com>
2018-03-29 18:03:12 +0900

    generator

--- GCode/Params.hs
+++ GCode/Params.hs
@@ -15,4 +15,20 @@ truncateTrail t
 
 pt t = truncateTrail (truncateTrail t)
 
-randp s = head $ take 1 $ randomRs ((fst limit), (snd limit)) (mkStdGen s) :: Double
+ptSin :: Double -> [Trail] -> [Trail]
+ptSin rad xs = map (\ x -> pt ((sin (fst x)) * rad, (sin (snd x)) * rad)) xs
+
+generateX n = [(x, 0) | x <- [0..n]]
+
+generateY n = [(0, y) | y <- [0..n]]
+
+translate' :: (Double, Double) -> [Trail] -> [Trail]
+translate' t ls = map (\ l -> ((fst l) + (fst t), (snd l) + (snd t))) ls
+
+sin' :: Double -> [Trail] -> [Trail]
+sin' rad = map (\ l -> ((fst l), (snd l))) ls
+
+randomize :: Double -> [Trail] -> [Trail]
+randomize fact ls = map (\ l -> ((randp 1 fact) + (fst l), (randp (2 + 8) fact) + (snd l) )) ls
+
+randp s mx = head $ take 1 $ randomRs (0, mx) (mkStdGen s) :: Double
